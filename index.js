507a3904675dbd35b30d9b12aada70d0c740c543
Flavien G <44913995+fgaujard@users.noreply.github.com>
2023-11-28 16:54:42 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 
-const name = process.env.MY_NAME;
-
 console.log(
   `I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`
 );
